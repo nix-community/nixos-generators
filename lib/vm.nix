bfc32d60380b4ed6c02facae31ffed628a34f333
Ingolf Wagner <palo@pepe>
2018-08-11 20:37:41 +0200

    forward ssh port

--- lib/vm.nix
+++ lib/vm.nix
@@ -7,6 +7,6 @@
 
   virtualisation.qemu.networkingOptions = [
     "-net nic,netdev=user.0,model=virtio"
-    "-netdev user,id=user.0\${QEMU_NET_OPTS:+,$QEMU_NET_OPTS},hostfwd=tcp::8088-:80"
+    "-netdev user,id=user.0\${QEMU_NET_OPTS:+,$QEMU_NET_OPTS},hostfwd=tcp::8088-:80,hostfwd=tcp::8022-:22"
   ];
 }
