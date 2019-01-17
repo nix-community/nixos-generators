147d26d8df308e31bc0471c52d9302ef383bbc00
lassulus <lassulus@lassul.us>
2019-01-17 21:54:58 +0100

    formats/qcow: set grub.device with mkDefault

--- formats/qcow.nix
+++ formats/qcow.nix
@@ -7,7 +7,7 @@
 
   boot.growPartition = true;
   boot.kernelParams = [ "console=ttyS0" ];
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.device = lib.mkDefault "/dev/vda";
   boot.loader.timeout = 0;
 
 
