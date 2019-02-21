c54ce47d5d9e50581222f3e791ac057391770a58
lassulus <lassulus@lassul.us>
2019-02-21 03:18:48 +0100

    openstack: rename nova-image to openstack-image

--- formats/openstack.nix
+++ formats/openstack.nix
@@ -1,8 +1,13 @@
-{ modulesPath, ... }:
-{
+{ modulesPath, lib, ... }:
+if lib.pathExists "${toString modulesPath}/../maintainers/scripts/openstack/nova-image.nix" then {
   imports = [
     "${toString modulesPath}/../maintainers/scripts/openstack/nova-image.nix"
   ];
 
   formatAttr = "novaImage";
+} else {
+  imports = [
+    "${toString modulesPath}/../maintainers/scripts/openstack/openstack-image.nix"
+  ];
+  formatAttr = "openstackImage";
 }
