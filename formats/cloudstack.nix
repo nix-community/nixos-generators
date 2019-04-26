5c8b2165a3e2c49aadd5d786d0c91596a436650c
Thomas Kim Pham <thomas.pham@ithings.ch>
2019-04-26 09:11:13 +0200

    add cloudstack image generator

---
+++ formats/cloudstack.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, modulesPath, ... }:
+{
+  imports = [
+    "${toString modulesPath}/virtualisation/cloudstack-config.nix"
+  ];
+
+  system.build.cloudstackImage =  import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    diskSize = 8192;
+    format = "qcow2";
+    configFile = pkgs.writeText "configuration.nix"
+      ''
+        {
+          imports = [ <nixpkgs/nixos/modules/virtualisation/cloudstack-config.nix> ];
+        }
+      '';
+  };
+
+  formatAttr = "cloudstackImage";
+}
