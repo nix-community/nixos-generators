3ede4cca3d82bf61e164100c79917cb8c50e2087
lassulus <lassulus@lassul.us>
2021-10-17 12:42:31 +0200

    formats/vm: remove conflicting port forwarding

--- formats/vm.nix
+++ formats/vm.nix
@@ -4,10 +4,5 @@
     "${toString modulesPath}/virtualisation/qemu-vm.nix"
   ];
 
-  virtualisation.qemu.networkingOptions = [
-    "-net nic,netdev=user.0,model=virtio"
-    "-netdev user,id=user.0\${QEMU_NET_OPTS:+,$QEMU_NET_OPTS},hostfwd=tcp::8088-:80,hostfwd=tcp::8022-:22"
-  ];
-
   formatAttr = "vm";
 }
