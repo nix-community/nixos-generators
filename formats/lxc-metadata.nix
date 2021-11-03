95d2f83c5d2b38ecf3fa599b4b4fd73486df0217
Maciej Krüger <mkg20001@gmail.com>
2021-11-03 17:43:48 +0100

    lxc: use new files from nixpkgs directly

--- formats/lxc-metadata.nix
+++ formats/lxc-metadata.nix
@@ -1,18 +1,9 @@
 { config, pkgs, modulesPath, ... }:
+
 {
-  system.build.metadata = pkgs.callPackage "${toString modulesPath}/../lib/make-system-tarball.nix" {
-    contents = [{
-      source = pkgs.writeText "metadata.yaml" ''
-        architecture: x86_64
-        creation_date: 1424284563
-        properties:
-          description: NixOS
-          os: NixOS
-          release: ${config.system.stateVersion}
-      '';
-      target = "/metadata.yaml";
-    }];
-  };
+  imports = [
+    "${toString modulesPath}/virtualisation/lxc-container.nix"
+  ];
 
   formatAttr = "metadata";
   filename = "*/tarball/*.tar.xz";
