5b422c3cb0a54482c07094fc797aae021124bbb4
lassulus <lassulus@lassul.us>
2022-08-14 16:57:55 +0200

    qcow: enable biosboot only for x86

--- formats/qcow.nix
+++ formats/qcow.nix
@@ -13,7 +13,13 @@
 
   boot.growPartition = true;
   boot.kernelParams = [ "console=ttyS0" ];
-  boot.loader.grub.device = lib.mkDefault "/dev/vda";
+  boot.loader.grub.device = if (pkgs.stdenv.system == "x86_64-linux") then
+    (lib.mkDefault "/dev/vda")
+  else
+    (lib.mkDefault "nodev");
+
+  boot.loader.grub.efiSupport = lib.mkIf (pkgs.stdenv.system != "x86_64-linux") (lib.mkDefault true);
+  boot.loader.grub.efiInstallAsRemovable = lib.mkIf (pkgs.stdenv.system != "x86_64-linux") (lib.mkDefault true);
   boot.loader.timeout = 0;
 
 
@@ -21,6 +27,7 @@
     inherit lib config pkgs;
     diskSize = 8192;
     format = "qcow2";
+    partitionTableType = "hybrid";
   };
 
   formatAttr = "qcow";
