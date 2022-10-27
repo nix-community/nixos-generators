1f311a13e108c9ab63d8d3f1efe40680a6af21f1
David Arnold <david.arnold@iohk.io>
2022-10-27 13:14:41 -0500

    fix: format module identity

--- format-module.nix
+++ format-module.nix
@@ -1,4 +1,11 @@
-{ lib, ... }: {
+{ lib, ... }: rec {
+  _file = ./format-module.nix;
+  # This deliberate key makes sure this module will be deduplicated
+  # regardless of the accessor path: either via flake's nixosModule
+  # or as part of the nixos-generate command. These two store paths
+  # of the module may differ and hence don't serve as a key
+  key = "github:nix-community/nixos-generators/format-module.nix";
+
   options = {
     filename = lib.mkOption {
       type = lib.types.str;
