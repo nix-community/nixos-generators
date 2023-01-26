7b19ba9c21d2a4dd3289f54f793683d51a5ee967
Tyler Langlois <tjl@byu.net>
2023-01-26 10:31:59 -0700

    Parameterize the configurable disk image size

--- formats/qcow-efi.nix
+++ formats/qcow-efi.nix
@@ -5,32 +5,42 @@
     "${toString modulesPath}/profiles/qemu-guest.nix"
   ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    autoResize = true;
-    fsType = "ext4";
+  options = {
+    diskSize = lib.mkOption {
+      default = "auto";
+      description = "The disk size in megabytes of the system disk image.";
+      type = with lib.types; oneOf [ ints.positive (enum [ "auto" ])];
+    };
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/ESP";
-    fsType = "vfat";
+  config = {
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      autoResize = true;
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
+
+    boot.growPartition = true;
+    boot.kernelParams = [ "console=ttyS0" ];
+    boot.loader.grub.device = "nodev";
+
+    boot.loader.grub.efiSupport = true;
+    boot.loader.grub.efiInstallAsRemovable = true;
+    boot.loader.timeout = 0;
+
+    system.build.qcow = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+      inherit lib config pkgs;
+      diskSize = config.diskSize;
+      format = "qcow2";
+      partitionTableType = "efi";
+    };
+
+    formatAttr = "qcow";
   };
-
-  boot.growPartition = true;
-  boot.kernelParams = [ "console=ttyS0" ];
-  boot.loader.grub.device = "nodev";
-
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.timeout = 0;
-
-  system.build.qcow = import "${toString modulesPath}/../lib/make-disk-image.nix" {
-    inherit lib config pkgs;
-    diskSize = 8192;
-    format = "qcow2";
-    partitionTableType = "efi";
-  };
-
-  formatAttr = "qcow";
 }
 
