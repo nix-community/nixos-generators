55f78d04fcc54912f188e1aa50671adc4f9ae9c7
DavHau <hsngrmpf+github@gmail.com>
2023-05-29 09:34:51 +0200

    add formatter to flake.nix

--- flake.nix
+++ flake.nix
@@ -28,11 +28,11 @@
     # }
 
     nixosGenerate = { pkgs ? null, lib ? nixpkgs.lib, format, system ? null, specialArgs ? { }, modules ? [ ], customFormats ? {} }:
-    let 
-      extraFormats = lib.mapAttrs' (name: value: lib.nameValuePair 
-        (name) 
-        (value // { 
-          imports = ( value.imports or [] ++ [ ./format-module.nix ] ); 
+    let
+      extraFormats = lib.mapAttrs' (name: value: lib.nameValuePair
+        (name)
+        (value // {
+          imports = ( value.imports or [] ++ [ ./format-module.nix ] );
         } )
         ) customFormats;
       formatModule = builtins.getAttr format (self.nixosModules // extraFormats);
@@ -57,6 +57,8 @@
     let
        forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin" ];
     in {
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
       packages = forAllSystems (system: let
         pkgs = nixpkgs.legacyPackages."${system}";
