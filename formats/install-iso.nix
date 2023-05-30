859c5d584c4695366d64747f53dae34eee5ce611
lassulus <git@lassul.us>
2023-05-30 10:14:57 +0200

    formats install-iso: remove isoName

--- formats/install-iso.nix
+++ formats/install-iso.nix
@@ -4,9 +4,6 @@
     "${toString modulesPath}/installer/cd-dvd/installation-cd-base.nix"
   ];
 
-  # for installer
-  isoImage.isoName = lib.mkDefault "nixos.iso";
-
   # override installation-cd-base and enable wpa and sshd start at boot
   systemd.services.wpa_supplicant.wantedBy = lib.mkForce [ "multi-user.target" ];
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
