0efc5559e84180322e00612dec9985de134b123a
Jörg Thalheim <joerg@thalheim.io>
2024-06-14 10:13:44 +0200

    test case where multiple grub.devices are set

--- formats/do.nix
+++ formats/do.nix
@@ -1,6 +1,9 @@
-{modulesPath, ...}: {
+{modulesPath, lib, ...}: {
   imports = [
     "${toString modulesPath}/virtualisation/digital-ocean-image.nix"
+    {
+      config.boot.loader.grub.devices = lib.mkForce [ "/dev/vda" "/dev/vdb" ];
+    }
   ];
 
   formatAttr = "digitalOceanImage";
