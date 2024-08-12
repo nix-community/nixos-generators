34c7afc9d9483b058d973771fb5cf52b641cd429
Jörg Thalheim <joerg@thalheim.io>
2024-08-12 08:27:53 +0200

    default.nix: quote urls

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-(import (builtins.fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+(import (builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
   src = ./.;
 })
 .defaultNix
