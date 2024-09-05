b5e20d6a0b7d567be26660916c27f6d4c409abbd
phaer <hello@phaer.org>
2024-09-05 14:54:51 +0200

    linode: use upstream linux-image.nix

--- formats/linode.nix
+++ formats/linode.nix
@@ -6,13 +6,13 @@
   ...
 }: {
   imports = [
-    "${toString modulesPath}/profiles/qemu-guest.nix"
+    "${toString modulesPath}/virtualisation/linode-image.nix"
   ];
 
-  formatAttr = "linode";
+  formatAttr = "linodeImage";
   fileExtension = ".img.gz";
 
-  system.build.linode = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+  system.build.linodeImage = import "${toString modulesPath}/../lib/make-disk-image.nix" {
     inherit lib config pkgs;
     partitionTableType = "none";
     format = "raw";
@@ -21,46 +21,13 @@
     '';
   };
 
-  # Set up filesystems according to Linode preference:
-  fileSystems."/" = {
-    device = "/dev/sda";
-    fsType = "ext4";
-    autoResize = true;
-  };
-
-  swapDevices = [{device = "/dev/sdb";}];
-
   # Enable LISH and Linode booting w/ GRUB
   boot = {
-    # Add kernel modules detected by nixos-generate-config:
-    initrd.availableKernelModules = [
-      "virtio_pci"
-      "virtio_scsi"
-      "ahci"
-      "sd_mod"
-    ];
-
     growPartition = true;
 
-    # Set up LISH serial connection:
-    kernelParams = ["console=ttyS0,19200n8"];
-
     loader = {
-      # Increase timeout to allow LISH connection:
-      timeout = lib.mkForce 10;
-
       grub = {
-        enable = true;
-        forceInstall = true;
-        device = "nodev";
         fsIdentifier = "label";
-
-        # Allow serial connection for GRUB to be able to use LISH:
-        extraConfig = ''
-          serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
-          terminal_input serial;
-          terminal_output serial
-        '';
 
         # Link /boot/grub2 to /boot/grub:
         extraInstallCommands = ''
@@ -78,17 +45,11 @@
 
   # Install diagnostic tools for Linode support:
   environment.systemPackages = with pkgs; [
-    inetutils
-    mtr
-    sysstat
     linode-cli
   ];
 
   networking = {
     enableIPv6 = true;
-    tempAddresses = "disabled";
-    useDHCP = true;
-    usePredictableInterfaceNames = false;
     interfaces.eth0 = {
       tempAddress = "disabled";
       useDHCP = true;
