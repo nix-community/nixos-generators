3e2727b18d0bb3d3b68ca9b4e6a174eeb8d827f1
phaer <hello@phaer.org>
2024-09-05 11:55:10 +0200

    Set default diskSize for vm formats

--- formats/vm.nix
+++ formats/vm.nix
@@ -1,7 +1,11 @@
-{modulesPath, ...}: {
+{
+  lib,
+  modulesPath,
+  ...
+}: {
   imports = [
     "${toString modulesPath}/virtualisation/qemu-vm.nix"
   ];
-
+  virtualisation.diskSize = lib.mkDefault (2 * 1024);
   formatAttr = "vm";
 }
