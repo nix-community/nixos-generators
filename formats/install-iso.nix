85c198f7c06a69db30d4f04aca30630f391f89a6
phaer <hello@phaer.org>
2024-09-05 15:10:43 +0200

    iso: squashfsCompression defaults to zstd upstream
    zstd -Xcompression-level 19

--- formats/install-iso.nix
+++ formats/install-iso.nix
@@ -11,9 +11,6 @@
   systemd.services.wpa_supplicant.wantedBy = lib.mkForce ["multi-user.target"];
   systemd.services.sshd.wantedBy = lib.mkForce ["multi-user.target"];
 
-  # Much faster than xz
-  isoImage.squashfsCompression = lib.mkDefault "zstd";
-
   formatAttr = "isoImage";
   fileExtension = ".iso";
 }
