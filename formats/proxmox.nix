0351342d7fd31ca0c73146e8948c40b9a684add6
Alexander Reinthal <email@reinthal.me>
2024-09-23 14:56:35 +0200

    feat: expose cloudInit options

--- formats/proxmox.nix
+++ formats/proxmox.nix
@@ -7,7 +7,14 @@
     "${toString modulesPath}/virtualisation/proxmox-image.nix"
   ];
 
-  proxmox.qemuConf.diskSize = specialArgs.diskSize or "auto";
+  proxmox = {
+    qemuConf.diskSize = specialArgs.diskSize or "auto";
+    cloudInit = {
+      enable = specialArgs.enableCloudInit or "auto";
+      defaultStorage = specialArgs.defaultStorage or "auto";
+      device = specialArgs.device "auto";
+    };
+  };
 
   formatAttr = "VMA";
   fileExtension = ".vma.zst";
