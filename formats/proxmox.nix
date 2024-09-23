0cb06939ea61ba86bd79b3f42e73848b3b2951c2
Alexander Reinthal <email@reinthal.me>
2024-09-23 15:31:07 +0200

    expose virtio0 harddrive default storage

--- formats/proxmox.nix
+++ formats/proxmox.nix
@@ -8,11 +8,17 @@
   ];
 
   proxmox = {
-    qemuConf.diskSize = specialArgs.diskSize or "auto";
+    qemuConf = {
+      diskSize = specialArgs.diskSize or "auto";
+
+      # Configuration for the default virtio disk. It can be used as a cue for PVE to autodetect the target storage.
+      # This parameter is required by PVE even if it isn't used.
+      virtio0 = specialArgs.virtio0 or "local-lvm:vm-9999-disk-0";
+    };
     cloudInit = {
-      enable = specialArgs.enableCloudInit or true; 
-      defaultStorage = specialArgs.defaultStorage or "local-lvm";
-      device = specialArgs.device or  "ide2";
+      enable = specialArgs.cloudInitEnable or true;
+      defaultStorage = specialArgs.cloudInitDefaultStorage or "local-lvm";
+      device = specialArgs.cloudInitDevice or "ide2";
     };
   };
 
